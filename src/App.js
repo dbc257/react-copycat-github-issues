a0612ea9a54ef5ade67a7a8bb796d80347424f3b
David Carlisle <dave_0021@yahoo.com>
2020-11-03 17:14:22 -0600

    secondary commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import IssuesList from './components/IssuesList';
+
+function App() {
+  return (
+    <div className="App">
+      <IssuesList />
+    </div>
+  );
+}
+
+export default App;
